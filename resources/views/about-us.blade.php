f0fd85dfa18ca42a04a0375fa0bb91fab6bef645
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2021-02-10 16:01:15 +0100

    Strutturato

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,8 @@
+@include('templates.head')
+@include('templates.header')
+@include('templates.main')
+<div class="links">
+        <a href="/">Home</a>
+    </div>
+</div>
+@include('templates.footer')
